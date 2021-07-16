da97162eafd729363a13cf1802680ef647fe089f
2214631535@qq.com <2214631535@qq.com>
2021-07-16 21:07:17 +0800

    master

--- src/components/manage/index.js
+++ src/components/manage/index.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
-import { Link, Route } from 'react-router-dom'
+import { Link, Route, Redirect } from 'react-router-dom'
 import "./index.css"
 import { Layout, Menu } from 'antd';
 import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
 import Reason from '../reason'
 import Noticomment from '../noticomment'
 import Data from '../data'
+import Reasondetail from '../reson_detail';
+import User from '../user';
 
 const { Content, Sider } = Layout;
 export default class Manage extends Component {
+    componentDidMount() {
+        if (sessionStorage.getItem('token') === 'undefined' || sessionStorage.getItem('token') === '' || sessionStorage.getItem('token') === null) {
+            this.props.history.push('/login1')
+
+        }
+
+
+    }
+
+    out = () => {
+        sessionStorage.setItem('token', "")
+        this.removeItem('token')
+        this.props.history.push('/login1')
+    }
+
     render() {
         return (
             <div>
@@ -24,7 +41,7 @@ export default class Manage extends Component {
                         }}
                     >
                         <div className="logo" />
-                        <Menu mode="inline" defaultSelectedKeys={['4']} style={{ marginTop: 25 }} >
+                        <Menu mode="inline" defaultSelectedKeys={['4']} style={{ marginTop: 25, }} >
                             <Menu.Item key="1" icon={<UserOutlined />}>
                                 <Link to="/manage/reason">
                                     文章资源管理与发布
@@ -35,30 +52,36 @@ export default class Manage extends Component {
                                     公告与留言厅管理
                                 </Link>
                             </Menu.Item>
-                            <Menu.Item key="3" icon={<UploadOutlined />}>
+                            <Menu.Item key="3" icon={<VideoCameraOutlined />}>
+                                <Link to="/manage/user">
+                                    用户信息管理
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="4" icon={<UploadOutlined />}>
                                 <Link to="/manage/data">
                                     实时数据监测
                                 </Link>
                             </Menu.Item>
-
                         </Menu>
                     </Sider>
                     <Layout>
-                        <Menu mode="horizontal" defaultSelectedKeys={['2']}>
+                        <Menu mode="horizontal" defaultSelectedKeys={['2']} >
                             <Menu.Item key="1"><h2>网站管理后台</h2></Menu.Item>
+                            <a href='xx' onClick={this.out}>退出登录</a>
                         </Menu>
                         <Content style={{ margin: '24px 16px 0', width: "100%", overflow: 'initial' }}>
                             <div style={{ padding: 24, minHeight: 650 }}>
                                 <Route path="/manage/reason" component={Reason} />
                                 <Route path="/manage/noticomment" component={Noticomment} />
                                 <Route path="/manage/data" component={Data} />
-
+                                <Route path="/manage/user" component={User} />
+                                <Route path="/manage/reson_detail/:id" component={Reasondetail} />
+                                <Redirect to="/manage/reason" />
                             </div>
                         </Content>
-
                     </Layout>
                 </Layout>,
-            </div>
+            </div >
         )
     }
 }
